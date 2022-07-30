191de4080dec459b42afce94162a4a10c7ce9303
kristiyanCgn <101466719+kristiyanCgn@users.noreply.github.com>
2022-07-30 12:38:00 +0200

    add profile context

---
+++ client/src/Contexts/profileContext.js
@@ -0,0 +1,40 @@
+import { useState, useEffect, useContext } from "react";
+
+import { createContext } from "react";
+import { appContext } from "./appContext";
+import * as profileService from '../services/profileService';
+
+export const profileContext = createContext();
+
+export const ProfileProvider = ({ children }) => {
+
+    const { isLoading, setIsLoading } = useContext(appContext);
+    
+    const [userDrivers, setUserDrivers] = useState([]);
+
+        const deleteUserDriver = (recordId) => {
+            setUserDrivers(state => state.filter(x => x._id !== recordId));
+        }
+
+        const addUserDriver = (recordData) => {
+            setUserDrivers(state => [
+                        ...state,
+                        recordData
+            ])
+        }
+    
+    useEffect(() => {
+        setIsLoading(true);
+        profileService.getAll()
+          .then(result => {
+            setIsLoading(false);
+            setUserDrivers(result)
+          })
+      }, [])
+
+    return (
+        <profileContext.Provider value={{ userDrivers, deleteUserDriver, addUserDriver }}>
+            {children}
+        </profileContext.Provider>
+    );
+}
